e6f3b42d903e355484df9bcda836668df00a813b
Yury Benesh <ybxsoft@tut.by>
2020-11-02 00:40:05 +0300

    added usage of builtins lib

--- cbackend/builtins_usage/src/hello.c
+++ cbackend/builtins_usage/src/hello.c
@@ -5,9 +5,9 @@
 int main(int argc, char const *argv[])
 {
     #ifdef NDEBUG
-    printf("No debug\n");
+    printf("No debug\n%p\n", miod_class_instance_from_interface);
     #else
-    printf("With debug\n");
+    printf("With debug\n%p\n", miod_class_instance_from_interface);
     #endif
     return 0; //miod_builtins(argc, argv);
 }
