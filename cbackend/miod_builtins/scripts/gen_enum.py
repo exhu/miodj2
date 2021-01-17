53f8d18eae802716741a5d4aff5df0b7156771ae
Yury Benesh <ybxsoft@tut.by>
2021-01-17 12:47:56 +0300

    gen_common.py, if_not_empty

--- cbackend/miod_builtins/scripts/gen_enum.py
+++ cbackend/miod_builtins/scripts/gen_enum.py
@@ -1,27 +1,11 @@
 # generates class, instance, enum, constructors definitions for header and C implementation.
-import textwrap
+from gen_common import *
 
 template = {
     'name': "NoName",
+    #'variants': {'Ok': '', 'Err': ''}, # variants key = name, value = text of C type var or None
     'variants': {'Ok': 'miod_BaseClassInstance *ok', 'Err': 'miod_BaseClassInstance *error'}, # variants key = name, value = text of C type var or None
 }
-
-INDENT = '    '
-COMMA_NL = ',\n'
-SEMICOLON_NL = ';\n'
-
-def join_with_indent(lines, indent_count, line_sep):
-    lines = list(lines)
-    lines_indented = [lines[0]]
-    lines_indented.extend(map(lambda x: INDENT*indent_count + x, lines[1:]))
-    return line_sep.join(lines_indented)
-
-
-def indent_rest(text, indent_count):
-    lines = text.split('\n')
-    lines_indented = [lines[0]]
-    lines_indented.extend(map(lambda x: INDENT*indent_count + x, lines[1:]))
-    return '\n'.join(lines_indented)
 
 
 def gen_variant_init_proc_hdr(cls, name, args_text):
@@ -36,7 +20,7 @@ def gen_union(variants, indent_count):
         return indent_rest(f'''union {{
     {join_with_indent(variants, 1, SEMICOLON_NL)};
 }};''', indent_count)
-    return ''
+    return EMPTY
 
 
 def gen_header(template):
@@ -45,7 +29,7 @@ def gen_header(template):
     enum_consts = map(gen_variant_enum_const, [x for x in variants.keys()])
     variants_union = variants.values()
     constructors = map(lambda pair: gen_variant_init_proc_hdr(name, pair[0], pair[1]), variants.items())
-    return f"""
+    return f"""/// header code
 extern miod_Class miod_cls_{name};
 
 typedef enum {{
@@ -54,8 +38,7 @@ typedef enum {{
 
 typedef struct {{
     miod_BaseClassInstance base;
-    miod_{name}Tag enum_tag;
-    {gen_union(variants_union, 1)}
+    miod_{name}Tag enum_tag;{if_not_empty(gen_union(variants_union, 1), NL+INDENT)}
 }} miod_Result;
 
 
@@ -64,5 +47,10 @@ typedef struct {{
 {join_with_indent(constructors, 0, SEMICOLON_NL)};
     """
 
+
+def gen_impl(template):
+    return f"""/// implementation code
+    """
+
 # ---
-print(gen_header(template))
+print(gen_header(template) + NL + gen_impl(template))
